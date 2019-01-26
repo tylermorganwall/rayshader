8df8f8da54457bb2adbc78577a08980ee974c58c
Tyler <tylermw@gmail.com>
2019-01-26 13:38:56 -0500

    Add `render_snapshot()` function to make capturing 3D views easier

---
+++ R/render_snapshot.R
@@ -0,0 +1,24 @@
+#'@title Render Snapshot of 3D Visualization
+#'
+#'@description Either captures the current rgl view and displays, or saves the current view to disk. 
+#'
+#'@param filename Filename of snapshot. If missing, will display to current device.
+#'@return Displays snapshot of current rgl plot (or saves to disk).
+#'@export
+#'@examples
+#'montereybay %>%
+#'  sphere_shade() %>%
+#'  plot_3d(montereybay,zscale=50,zoom=0.6,theta=-90)
+#'  
+#'render_snapshot()
+#'rgl::rgl.clear()
+render_snapshot = function(filename) {
+  temp = paste0(tempfile(),".png")
+  rgl::snapshot3d(filename=temp)
+  tempmap = png::readPNG(temp)
+  if(missing(filename)) {
+    plot_map(tempmap)
+  } else {
+    save_png(tempmap,filename)
+  }
+}
