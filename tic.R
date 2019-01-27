6d9c6952b0f888a789e1ea250b4dfa1cf68f4fa9
Tyler <tylermw@gmail.com>
2019-01-27 10:10:15 -0500

    24 bit xvfb

--- tic.R
+++ tic.R
@@ -1,6 +1,6 @@
 get_stage("before_install") %>%
   add_code_step(options(rgl.useNULL = TRUE)) %>%
-  add_code_step(system("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"))
+  add_code_step(system("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24"))
 
 get_stage("before_script") %>%
   add_code_step(system("export DISPLAY=:99.0")) %>%
