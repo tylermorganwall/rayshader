51624ca8e73d0b0b8d0e1db6238e6b17346a5d30
Tyler <tylermw@gmail.com>
2019-01-26 22:22:23 -0500

    lower case xvfb

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 get_stage("before_install") %>%
-  add_code_step(system('/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24"'))
+  add_code_step(system('/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/xvfb -- :99 -ac -screen 0 1280x1024x24"'))
 
 add_package_checks()
 
