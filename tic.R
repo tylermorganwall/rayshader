9db8b1e1b026ea2ac1ac9d3709c12c2cded9eee9
Tyler <tylermw@gmail.com>
2019-01-26 21:57:26 -0500

    Try start-stop daemon

--- tic.R
+++ tic.R
@@ -1,7 +1,5 @@
 get_stage("before_install") %>%
-  add_code_step(system('export DISPLAY=:1')) %>%
-  add_code_step(system('sh -e /etc/init.d/xvfb start')) %>%
-  add_code_step(system("sleep 3"))
+  add_code_step(system('/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24"')) %>%
 
 add_package_checks()
 
