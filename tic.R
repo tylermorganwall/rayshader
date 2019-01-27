d4d2124030abec013da6c281a1fe3fd58558e62d
Tyler <tylermw@gmail.com>
2019-01-26 22:57:39 -0500

    Try adding steps to before_script

--- tic.R
+++ tic.R
@@ -1,6 +1,11 @@
 get_stage("before_install") %>%
   add_code_step(system("/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16"))
 
+get_stage("before_script") %>%
+  add_code_step(system("export DISPLAY=:99.0")) %>%
+  add_code_step(system("sh -e /etc/init.d/xvfb start")) %>%
+  add_code_step(system("sleep 3"))
+
 add_package_checks()
 
 if (Sys.getenv("id_rsa") != "" && !ci()$is_tag()) {
