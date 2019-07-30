fb228118df68829878bf12e3a01cf7db302ec52b
Tyler <tylermw@gmail.com>
2019-07-30 18:14:25 -0400

    rayshader 0.12.0: render_movie() updates
    -`render_movie()` added the ability to add and customize a title/text to a video with the `title_*` arguments
    -`render_movie()` added the ability to add an image overlay with the `image_overlay` argument.
    -`render_movie()` fixed bug with non-divisible-by-2 movie dimensions

--- R/render_movie.R
+++ R/render_movie.R
@@ -14,7 +14,21 @@
 #'@param theta Default `0`. Theta values, in degrees. 
 #'@param zoom Defaults to the current view. Zoom value, between `0` and `1`. 
 #'@param fov Defaults to the current view. Field of view values, in degrees.
-#'@param ... Additional parameters to pass to av::av_capture_graphics
+#'@param title_text Default `NULL`. Text. Adds a title to the movie, using magick::image_annotate. Change
+#'the position, color, and size by passing the appropriate argument to ... from magick::image_annotate.
+#'@param title_offset Default `c(20,20)`. Distance from the top-left (default, `gravity` direction in 
+#'image_annotate) corner to offset the title.
+#'@param title_size Default `30`. Font size in pixels.
+#'@param title_color Default `black`. Font color.
+#'@param title_font Default `sans`. String with font family such as "sans", "mono", "serif", "Times", "Helvetica", 
+#'"Trebuchet", "Georgia", "Palatino" or "Comic Sans".
+#'@param image_overlay Default `NULL`. Either a string indicating the location of a png image to overlay
+#'over the whole movie (transparency included), or a 4-layer RGBA array. This image will be resized to the 
+#'dimension of the movie if it does not match exactly.
+#'@param audio Default `NULL`. Optional file with audio to add to the video.
+#'@param progbar Default `TRUE` if interactive, `FALSE` otherwise. If `FALSE`, turns off progress bar. 
+#'Will display a progress bar when adding an overlay or title.
+#'@param ... Additional parameters to pass to magick::image_annotate. 
 #'@export
 #'@examples
 #'filename_movie = tempfile()
@@ -31,6 +45,7 @@
 #'filename_movie = tempfile()
 #'
 #'#You can change to an oscillating orbit. The magnification is increased and azimuth angle set to 30.
+#'#A title has also been added using the title_text argument.
 #'\donttest{
 #'montereybay %>%
 #'  sphere_shade(texture="imhof1") %>%
@@ -38,7 +53,8 @@
 #'          waterlinecolor="white", waterlinealpha=0.5)
 #'#Un-comment the following to run:
 #'#render_movie(filename = filename_movie, type = "oscillate", 
-#'#             frames = 60,  phi = 30, zoom = 0.8, theta = -90)
+#'#             frames = 60,  phi = 30, zoom = 0.8, theta = -90,
+#'#             title_text = "Monterey Bay: Oscillating")
 #'}             
 #'filename_movie = tempfile()
 #'
@@ -60,12 +76,35 @@
 #'#             frames = 360,  phi = phivecfull, zoom = zoomvecfull, theta = thetavec)
 #'}
 render_movie = function(filename, type = "orbit", frames = 360, fps = 30, 
-                        phi = 30, theta = 0, zoom = NULL, fov = NULL, ...) {
+                        phi = 30, theta = 0, zoom = NULL, fov = NULL, 
+                        title_text = NULL, title_offset = c(20,20), 
+                        title_color = "black", title_size = 30, title_font = "sans",
+                        image_overlay = NULL, audio=NULL, progbar = interactive(), ...) {
   if(!("av" %in% rownames(utils::installed.packages()))) {
     stop("`av` package required for render_movie()")
   }
   if(is.null(filename)) {
     stop("render_movie requires a filename")
+  }
+  if(!is.null(title_text)) {
+    has_title = TRUE
+  } else {
+    has_title = FALSE
+  }
+  if(length(title_offset) != 2) {
+    stop("`title_offset` needs to be length-2 vector")
+  }
+  if(!is.null(image_overlay)) {
+    if("character" %in% class(image_overlay)) {
+      image_overlay_file = image_overlay
+      has_overlay = TRUE
+    } else if("array" %in% class(image_overlay)) {
+      image_overlay_file = tempfile()
+      png::writePNG(image_overlay_file)
+      has_overlay = TRUE
+    }
+  } else {
+    has_overlay = FALSE
   }
   if(substring(filename, nchar(filename)-3,nchar(filename)) != ".mp4") {
     filename = paste0(filename,".mp4")
@@ -106,5 +145,53 @@ render_movie = function(filename, type = "orbit", frames = 360, fps = 30,
   } else {
     stop("Unknown type: ", type)
   }
-  av::av_encode_video(png_files, output = filename, framerate = fps, vfilter = "scale=-2:-2", ...)
+  temp = png::readPNG(png_files[1])
+  dimensions = dim(temp)
+  if(dimensions[1] %% 2 != 0) {
+    dimensions[1] = dimensions[1] - 1
+  }
+  if(has_overlay) {
+    if(!("magick" %in% rownames(utils::installed.packages()))) {
+      stop("`magick` package required for adding overlay")
+    }
+    if(progbar) {
+      pb = progress::progress_bar$new(
+        format = "  Adding overlay image [:bar] :percent eta: :eta",
+        total = frames, width= 60)
+    }
+    for(i in seq_len(frames)) {
+      if(progbar) {
+        pb$tick()
+      }
+      magick::image_read(png_files[i]) %>%
+        magick::image_composite(
+          magick::image_scale(magick::image_read(image_overlay_file),
+                      paste0(dimensions[1],"x",dimensions[2]))
+          ) %>%
+        magick::image_write(path = png_files[i], format = "png")
+    }
+  }
+  if(has_title) {
+    if(!("magick" %in% rownames(utils::installed.packages()))) {
+      stop("`magick` package required for adding title")
+    }
+    if(progbar) {
+      pb = progress::progress_bar$new(
+        format = "  Adding title text [:bar] :percent eta: :eta",
+        total = frames, width= 60)
+    }
+    for(i in seq_len(frames)) {
+      if(progbar) {
+        pb$tick()
+      }
+      magick::image_read(png_files[i]) %>%
+        magick::image_annotate(title_text, 
+                       location = paste0("+", title_offset[1],"+",title_offset[2]),
+                       size = title_size, color = title_color, 
+                       font = title_font, ...) %>%
+        magick::image_write(path = png_files[i], format = "png")
+    }
+  }
+  av::av_encode_video(png_files, output = filename, framerate = fps, 
+                      vfilter = paste0("scale=",dimensions[1],":-2"), audio=audio)
 }
