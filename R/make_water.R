6b011e41579362666b40399ae706c992d3842dc4
Tyler <tylermw@gmail.com>
2019-10-03 20:59:33 -0400

    Center make_water

--- R/make_water.R
+++ R/make_water.R
@@ -13,25 +13,31 @@ make_water = function(heightmap,waterheight=mean(heightmap),watercolor="lightblu
   heightmap = heightmap/zscale
   na_matrix = is.na(heightmap)
   waterheight = waterheight/zscale
-  heightlist = make_water_cpp(heightmap,na_matrix, waterheight)
-  fullsides = do.call(rbind,heightlist)
-  fullsides[,1] = (fullsides[,1] - nrow(heightmap)/2)
-  fullsides[,3] = -(fullsides[,3] + ncol(heightmap)/2 - 1)
-  fullsides = fullsides[nrow(fullsides):1,]
-  if(all(!na_matrix)) {
-    triangles3d(matrix(c(-nrow(heightmap)/2+1, nrow(heightmap)/2, -nrow(heightmap)/2+1,
-                         waterheight,waterheight,waterheight,
-                         ncol(heightmap)/2,-ncol(heightmap)/2+1,-ncol(heightmap)/2+1),3,3),
-                color=watercolor,alpha=wateralpha,front="filled",back="culled",texture=NULL,ambient = "#000003")
-    triangles3d(matrix(c(-nrow(heightmap)/2+1, nrow(heightmap)/2, nrow(heightmap)/2,
-                         waterheight,waterheight,waterheight,
-                         ncol(heightmap)/2,ncol(heightmap)/2,-ncol(heightmap)/2+1),3,3),
-                color=watercolor,alpha=wateralpha,front="filled",back="culled",texture=NULL,ambient = "#000003")
-    rgl::triangles3d(fullsides,lit=FALSE,color=watercolor,alpha=wateralpha,front="fill",depth_test="less",texture=NULL,ambient = "#000003")
+  if(all(heightmap >= waterheight)) {
+    warning("No water rendered--all elevations above or equal to water level. Range of heights: ",
+            min(heightmap,na.rm = TRUE)*zscale,"-", max(heightmap,na.rm = TRUE)*zscale, ". Depth specified: ",
+            waterheight * zscale)
   } else {
-    rgl::triangles3d(fullsides,lit=FALSE,color=watercolor,alpha=wateralpha,front="fill",texture=NULL,ambient = "#000003")
-    basemat = matrix(waterheight,nrow(heightmap),ncol(heightmap))
-    basemat[is.na(heightmap)] = NA
-    rgl.surface(1:nrow(basemat)-nrow(basemat)/2,1:ncol(basemat)-ncol(basemat)/2,basemat,color=watercolor,alpha=wateralpha,lit=FALSE,texture=NULL,ambient = "#000003")
+    heightlist = make_water_cpp(heightmap,na_matrix, waterheight)
+    fullsides = do.call(rbind,heightlist)
+    fullsides[,1] = (fullsides[,1] - nrow(heightmap)/2)
+    fullsides[,3] = -(fullsides[,3] + ncol(heightmap)/2 - 1)
+    fullsides = fullsides[nrow(fullsides):1,]
+    if(all(!na_matrix)) {
+      triangles3d(matrix(c(-nrow(heightmap)/2+1, nrow(heightmap)/2, -nrow(heightmap)/2+1,
+                           waterheight,waterheight,waterheight,
+                           ncol(heightmap)/2,-ncol(heightmap)/2+1,-ncol(heightmap)/2+1),3,3),
+                  color=watercolor,alpha=wateralpha,front="filled",back="culled",texture=NULL,ambient = "#000003")
+      triangles3d(matrix(c(-nrow(heightmap)/2+1, nrow(heightmap)/2, nrow(heightmap)/2,
+                           waterheight,waterheight,waterheight,
+                           ncol(heightmap)/2,ncol(heightmap)/2,-ncol(heightmap)/2+1),3,3),
+                  color=watercolor,alpha=wateralpha,front="filled",back="culled",texture=NULL,ambient = "#000003")
+      rgl::triangles3d(fullsides,lit=FALSE,color=watercolor,alpha=wateralpha,front="fill",depth_test="less",texture=NULL,ambient = "#000003")
+    } else {
+      rgl::triangles3d(fullsides,lit=FALSE,color=watercolor,alpha=wateralpha,front="fill",texture=NULL,ambient = "#000003")
+      basemat = matrix(waterheight,nrow(heightmap),ncol(heightmap))
+      basemat[is.na(heightmap)] = NA
+      rgl.surface(1:nrow(basemat)-nrow(basemat)/2,1:ncol(basemat)-ncol(basemat)/2,basemat,color=watercolor,alpha=wateralpha,lit=FALSE,texture=NULL,ambient = "#000003")
+    }
   }
 }
