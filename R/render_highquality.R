83f1438fb9c02190d667362055680e6f742c511d
Tyler <tylermw@gmail.com>
2019-11-11 16:02:42 -0500

    change docs for render_highquality() to \donttest{}

--- R/render_highquality.R
+++ R/render_highquality.R
@@ -43,7 +43,7 @@
 #'@export
 #'@examples
 #'#Render the volcano dataset using pathtracing
-#'\dontrun{
+#'\donttest{
 #'volcano %>%
 #'  sphere_shade() %>%
 #'  plot_3d(volcano,zscale = 2)
@@ -51,36 +51,36 @@
 #'}
 #'
 #'#Change position of light
-#'\dontrun{
+#'\donttest{
 #'render_highquality(lightdirection = 45)
 #'}
 #'
 #'#Change vertical position of light
-#'\dontrun{
+#'\donttest{
 #'render_highquality(lightdirection = 45, lightaltitude=10)
 #'}
 #'
 #'#Change the ground material
-#'\dontrun{
+#'\donttest{
 #'render_highquality(lightdirection = 45, lightaltitude=60, 
 #'                   ground_material = rayrender::diffuse(checkerperiod = 30, checkercolor="grey50"))
 #'}
 #'
 #'#Add three different color lights and a title
-#'\dontrun{
+#'\donttest{
 #'render_highquality(lightdirection = c(0,120,240), lightaltitude=45, 
 #'                   lightcolor=c("red","green","blue"), title_text = "Red, Green, Blue",
 #'                   title_bar_color="white", title_bar_alpha=0.8)
 #'}
 #'
 #'#Change the camera:
-#'\dontrun{
+#'\donttest{
 #'render_camera(theta=-45,phi=60,fov=60,zoom=0.8)
 #'render_highquality(lightdirection = c(0), 
 #'                   title_bar_color="white", title_bar_alpha=0.8)
 #'}
 #'#Add a shiny metal sphere
-#'\dontrun{
+#'\donttest{
 #'render_camera(theta=-45,phi=60,fov=60,zoom=0.8)
 #'render_highquality(lightdirection = c(0,120,240), lightaltitude=45, 
 #'                   lightcolor=c("red","green","blue"),
@@ -89,7 +89,7 @@
 #'}
 #'
 #'#Add a red light to the volcano and change the ambient light to dusk
-#'\dontrun{
+#'\donttest{
 #'render_camera(theta=45,phi=45)
 #'render_highquality(lightdirection = c(240), lightaltitude=30, 
 #'                   lightcolor=c("#5555ff"),
@@ -99,7 +99,7 @@
 #'                                                                implicit_sample=TRUE)))
 #'}
 #'#Manually change the camera location and direction
-#'\dontrun{
+#'\donttest{
 #'render_camera(theta=45,phi=45,fov=90)
 #'render_highquality(lightdirection = c(240), lightaltitude=30, lightcolor=c("#5555ff"), 
 #'                   camera_location = c(50,10,10), camera_lookat = c(0,15,0), 
