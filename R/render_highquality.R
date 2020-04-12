62d82a3d29ea034727b67bdc0c64b8f9d1619af6
Tyler <tylermw@gmail.com>
2020-04-11 23:56:06 -0400

    Add label and line support to render_highquality()

--- R/render_highquality.R
+++ R/render_highquality.R
@@ -19,6 +19,15 @@
 #'@param cache_filename Name of temporary filename to store OBJ file, if the user does not want to rewrite the file each time.
 #'@param width Defaults to the width of the rgl window. Width of the rendering. 
 #'@param height Defaults to the height of the rgl window. Height of the rendering. 
+#'@param text_angle Default `NULL`, which forces the text always to face the camera. If a single angle (degrees),
+#'will specify the absolute angle all the labels are facing. If three angles, this will specify all three orientations
+#'(relative to the x,y, and z axes) of the text labels.
+#'@param text_size Default `6`. Height of the text.
+#'@param text_offset Default `c(0,0,0)`. Offset to be applied to all text labels.
+#'@param line_radius Default `0.5`. Radius of the label line segments.
+#'@param scale_text_angle Default `NULL`. Same as `text_angle`, but for the scale bar.
+#'@param scale_text_size Default `6`. Height of the scale bar text.
+#'@param scale_text_offset Default `c(0,0,0)`. Offset to be applied to all scale bar text labels.
 #'@param title_text Default `NULL`. Text. Adds a title to the image, using magick::image_annotate. 
 #'@param title_offset Default `c(20,20)`. Distance from the top-left (default, `gravity` direction in 
 #'image_annotate) corner to offset the title.
@@ -109,6 +118,8 @@
 render_highquality = function(filename = NULL, light = TRUE, lightdirection = 315, lightaltitude = 45, lightsize=NULL,
                               lightintensity = 500, lightcolor = "white", obj_material = diffuse(),
                               cache_filename=NULL, width = NULL, height = NULL, 
+                              text_angle = NULL, text_size = 6, text_offset = c(0,0,0), line_radius=0.5,
+                              scale_text_angle = NULL, scale_text_size = 6, scale_text_offset = c(0,0,0), 
                               title_text = NULL, title_offset = c(20,20), 
                               title_color = "black", title_size = 30, title_font = "sans",
                               title_bar_color = NULL, title_bar_alpha = 0.5,
@@ -216,6 +227,108 @@ render_highquality = function(filename = NULL, light = TRUE, lightdirection = 31
     scene = obj_model(cache_filename, x = -bbox_center[1], y = -bbox_center[2], 
                       z = -bbox_center[3],
                       material = obj_material)
+  }
+  labelids = get_ids_with_labels(typeval = "raytext")$id
+  labels = list()
+  counter = 1
+  for(i in seq_len(length(labelids))) {
+    temp_label = rgl.attrib(labelids[i], "texts")
+    temp_center = rgl.attrib(labelids[i], "centers")
+    temp_color = rgl.attrib(labelids[i], "colors")
+    for(j in seq_len(nrow(temp_label))) {
+      labelfile = ""
+      if(no_cache) {
+        labelfile = paste0(temp_label[j,1],".png")
+      } else {
+        labelfile = tempfile(fileext = ".png")
+      }
+      rayimage::add_title(matrix(0,ncol = nchar(temp_label[j,1])*60, nrow=60), 
+                          title_size  = 60,
+                          title_offset = c(0,0),title_text = temp_label, title_color = "white",
+                          title_position = "center", filename = labelfile)
+      if(is.null(text_angle)) {
+        anglevec = c(rotmat[1],theta,0)
+      } else {
+        if(length(text_angle) == 1) {
+          anglevec = c(0,text_angle,0)
+        } else {
+          anglevec = text_angle
+        }
+      }
+      labels[[counter]] = xy_rect(x=temp_center[j,1] - bbox_center[1] + text_offset[1], 
+                                  y=temp_center[j,2] - bbox_center[2] + text_offset[2], 
+                                  z=temp_center[j,3] - bbox_center[3] + text_offset[3],
+                                  angle = anglevec,
+                                  xwidth = nchar(temp_label[j,1])*text_size, ywidth = text_size,
+                                  material = diffuse(color = temp_color[j,1:3], alpha_texture = labelfile))
+      counter = counter + 1
+    }
+  }
+  if(length(labels) > 0) {
+    all_labels = do.call(rbind, labels)
+    scene = add_object(scene, all_labels)
+  }
+  labellineids = get_ids_with_labels(typeval = "textline")$id
+  labelline = list()
+  counter = 1
+  for(i in seq_len(length(labellineids))) {
+    temp_verts = rgl.attrib(labellineids[i], "vertices")
+    temp_color = rgl.attrib(labellineids[i], "colors")
+    for(j in seq_len(nrow(temp_verts)/2)) {
+      labelline[[counter]] = segment(start = temp_verts[2*j-1,] - bbox_center, 
+                                     end   = temp_verts[2*j,] - bbox_center,
+                                     radius = line_radius,
+                                     material = diffuse(color = temp_color[j,1:3]))
+      counter = counter + 1
+    }
+  }
+  scalelabelids = get_ids_with_labels(typeval = "text_scalebar")$id
+  scalelabels = list()
+  counter = 1
+  for(i in seq_len(length(scalelabelids))) {
+    temp_label = rgl.attrib(scalelabelids[i], "texts")
+    temp_center = rgl.attrib(scalelabelids[i], "centers")
+    temp_color = rgl.attrib(scalelabelids[i], "colors")
+    for(j in seq_len(nrow(temp_label))) {
+      scalelabelfile = ""
+      if(no_cache) {
+        scalelabelfile = paste0(temp_label[j,1],".png")
+      } else {
+        scalelabelfile = tempfile(fileext = ".png")
+      }
+      rayimage::add_title(matrix(0,ncol = nchar(temp_label[j,1])*60, nrow=60), 
+                          title_size  = 60,
+                          title_offset = c(0,0),title_text = temp_label, title_color = "white",
+                          title_position = "center", filename = scalelabelfile)
+      if(is.null(text_angle)) {
+        anglevec = c(rotmat[1],theta,0)
+      } else {
+        if(length(text_angle) == 1) {
+          anglevec = c(0,text_angle,0)
+        } else {
+          anglevec = text_angle
+        }
+      }
+      scalelabels[[counter]] = xy_rect(x=temp_center[j,1] - bbox_center[1] + scale_text_offset[1], 
+                                  y=temp_center[j,2] - bbox_center[2] + scale_text_offset[2], 
+                                  z=temp_center[j,3] - bbox_center[3] + scale_text_offset[3],
+                                  angle = anglevec,
+                                  xwidth = nchar(temp_label[j,1])*scale_text_size, ywidth = scale_text_size,
+                                  material = diffuse(color = temp_color[j,1:3], alpha_texture = scalelabelfile))
+      counter = counter + 1
+    }
+  }
+  if(length(labels) > 0) {
+    all_labels = do.call(rbind, labels)
+    scene = add_object(scene, all_labels)
+  }
+  if(length(labelline) > 0) {
+    all_labellines = do.call(rbind, labelline)
+    scene = add_object(scene, all_labellines)
+  }
+  if(length(scalelabels) > 0) {
+    all_scalelabels = do.call(rbind, scalelabels)
+    scene = add_object(scene, all_scalelabels)
   }
   if(has_shadow) {
     scene = add_object(scene, xz_rect(zwidth=ground_size,xwidth=ground_size,y=shadowdepth-bbox_center[2], material = ground_material))
