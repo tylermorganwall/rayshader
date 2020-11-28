874d42afb00cca6373730ee0fba85e73502a01af
Tyler <tylermw@gmail.com>
2020-11-28 13:52:58 -0500

    -Update docs and README

--- R/render_polygons.R
+++ R/render_polygons.R
@@ -1,8 +1,7 @@
-#'@title Render Path
+#'@title Render Polygons
 #'
-#'@description Adds a 3D path to the current scene, using latitude/longitude or coordinates in the reference
-#'system defined by the extent object. If no altitude is provided, the path will be elevated a constant offset 
-#'above the heightmap. If the path goes off the edge, the nearest height on the heightmap will be used.
+#'@description Adds 3D polygons to the current scene, using latitude/longitude or coordinates in the reference
+#'system defined by the extent object. 
 #'
 #' @param polygon `sf` object, "SpatialPolygon" `sp` object,  or xy coordinates
 #' of polygon represented in a way that can be processed by `xy.coords()`.  If
@@ -32,35 +31,45 @@
 #' @param light_altitude Default `c(45, 60)`. Degree(s) from the horizon from which to light the polygons.
 #' @param light_direction Default `c(45, 60)`. Degree(s) from north from which to light the polygons.
 #' @param light_intensity Default `0.3`. Intensity of the specular highlight on the polygons.
-#' @param clear_previous Default `FALSE`. If `TRUE`, it will clear all existing paths.
+#' @param clear_previous Default `FALSE`. If `TRUE`, it will clear all existing polygons.
 #' @export
 #' @examples
 #' \donttest{
-#' if("sf" %in% rownames(utils::installed.packages())) {
-#'    montereybay %>%
-#'      sphere_shade(texture = "desert") %>%
-#'      add_shadow(ray_shade(montereybay,zscale=50)) %>%
-#'      plot_3d(montereybay,water=TRUE, windowsize=800, watercolor="dodgerblue")
-#'    render_camera(theta=140,  phi=55, zoom = 0.85, fov=30)
-#'    #We will apply a negative buffer to create space between adjacent polygons:
-#'    render_polygons(sf::st_simplify(sf::st_buffer(monterey_counties_sf,-0.003), dTolerance=0.001), 
-#'                    extent = attr(montereybay,"extent"), top=10,
-#'                    parallel=TRUE)
-#'    render_snapshot()
-#'    render_polygons(clear_previous=TRUE)
-#'    
-#'    #We can set the height of the data to a column in the sf object: we'll use the land area.
-#'    #We'll have to scale this value because it's max value is 2.6 billion:
-#'    render_camera(theta=-60,  phi=60, zoom = 0.85, fov=30)
-#'    render_polygons(sf::st_simplify(sf::st_buffer(monterey_counties_sf,-0.003), dTolerance=0.001), 
-#'                    extent = attr(montereybay,"extent"), data_column_top = "ALAND",
-#'                    scale_data = 300/(2.6E9), color="chartreuse4",
-#'                    parallel=TRUE)
-#'    render_snapshot()        
-#'    #This function also works with `render_highquality()`
-#'    render_highquality(sample_method="stratified", samples=400, clamp_value=10)
-#'    rgl::rgl.close()
-#' }
+#' #Render the county borders as polygons in Monterey Bay
+#' montereybay %>%
+#'   sphere_shade(texture = "desert") %>%
+#'   add_shadow(ray_shade(montereybay,zscale=50)) %>%
+#'   plot_3d(montereybay,water=TRUE, windowsize=800, watercolor="dodgerblue")
+#' render_camera(theta=140,  phi=55, zoom = 0.85, fov=30)
+#' 
+#' #We will apply a negative buffer to create space between adjacent polygons:
+#' mont_county_buff = sf::st_simplify(sf::st_buffer(monterey_counties_sf,-0.003), dTolerance=0.001)
+#' 
+#' render_polygons(mont_county_buff, 
+#'                 extent = attr(montereybay,"extent"), top=10,
+#'                 parallel=TRUE)
+#' render_snapshot()
+#' 
+#' #We can specify the bottom of the polygons as well. Here I float the polygons above the surface
+#' #by specifying the bottom argument. We clear the previous polygons with `clear_previous = TRUE`.
+#' render_camera(theta=-60,  phi=20, zoom = 0.85, fov=0)
+#' render_polygons(mont_county_buff, 
+#'                 extent = attr(montereybay,"extent"), bottom = 190, top=200,
+#'                 parallel=TRUE,clear_previous=TRUE)
+#' render_snapshot()
+#' 
+#' #We can set the height of the data to a column in the sf object: we'll use the land area.
+#' #We'll have to scale this value because it's max value is 2.6 billion:
+#' render_camera(theta=-60,  phi=60, zoom = 0.85, fov=30)
+#' render_polygons(mont_county_buff, 
+#'                 extent = attr(montereybay,"extent"), data_column_top = "ALAND",
+#'                 scale_data = 300/(2.6E9), color="chartreuse4",
+#'                 parallel=TRUE,clear_previous=TRUE)
+#' render_snapshot()        
+#' 
+#' #This function also works with `render_highquality()`
+#' render_highquality(sample_method="stratified", samples=400, clamp_value=10)
+#' rgl::rgl.close()
 #' }
 render_polygons = function(polygon, extent,  color = "red", top = 1, bottom = NA,
                            data_column_top = NULL, data_column_bottom = NULL,
